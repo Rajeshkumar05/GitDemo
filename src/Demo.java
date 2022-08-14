a19585bf22c905771f1b226aa303222eb0a4d5d3
raselvam <raselvam@RASELVAM-54QTQ.partnet.cisco.com>
2022-08-14 07:57:41 +0530

    latest commit

--- src/Demo.java
+++ src/Demo.java
@@ -2,7 +2,8 @@
 public class Demo {
 
 	public static void main(String[] args) {
-		System.out.println("Demo1 file11111111111");
+		System.out.println("Demo1");
+		System.out.println("Demo1" );
 	}
 
 }
--- src/Demo2.java
+++ src/Demo2.java
@@ -2,8 +2,9 @@
 public class Demo2 {
 
 	public static void main(String[] args) {
-		System.out.println("demo2 file2222222222");
-
+		System.out.println("demo2");
+		System.out.println("demo2"
+				+ "");
 	}
 
 }
